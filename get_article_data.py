69cd2c10ac32af01e93cb9dee476cfa371a3df15
mkrnr <info@mkoerner.de>
2016-05-12 17:37:14 +0200

    Fix python process name in get_article_data.py

--- get_article_data.py
+++ get_article_data.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     max_python_processes = 10
 
     # get the number of running python processes that are currently running 
-    process_name = "python" 
+    process_name = "python2" 
     python_process_count = 0
     for proc in psutil.process_iter(): 
         process = psutil.Process(proc.pid)# Get the process info using PID
