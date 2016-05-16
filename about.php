330f83874f71fc085da7c9b9f59fe9fbe07f3fe5
mkrnr <info@mkoerner.de>
2016-05-16 17:21:16 +0200

    Add usage info to about.php

--- about.php
+++ about.php
@@ -34,24 +34,33 @@
       </p>
       <h4>Via URL Parameters</h4>
       <p>
-        It is possible to access the plotted results via URL parameters.
-      </p>
-      <p>
-        For the German Wikipedia article called <a href="https://de.wikipedia.org/wiki/Test">Test</a>, the URL look like this: <a href="http://wikiwhere.west.uni-koblenz.de/article.php?url=https://de.wikipedia.org/wiki/Test">http://wikiwhere.west.uni-koblenz.de/article.php?url=https://de.wikipedia.org/wiki/Test</a>
-      </p>
-      <p>
-        The URL parameter "new-crawl" allows to force a new analysis: <a href="http://wikiwhere.west.uni-koblenz.de/article.php?url=https://de.wikipedia.org/wiki/Test&new-crawl=true">http://wikiwhere.west.uni-koblenz.de/article.php?url=https://de.wikipedia.org/wiki/Test&new-crawl=true</a>
-      </p>
-      <p>
+        It is possible to access the plotted results via URL parameters:<br>
+        <code>http://wikiwhere.west.uni-koblenz.de/article.php?url=[article-url]</code><br>
+        For example, the German Wikipedia article <a href="https://de.wikipedia.org/wiki/Test">Test</a> can be accessed with:<br>
+        <a href="http://wikiwhere.west.uni-koblenz.de/article.php?url=https://de.wikipedia.org/wiki/Test">http://wikiwhere.west.uni-koblenz.de/article.php?url=https://de.wikipedia.org/wiki/Test</a><br>
+        The URL parameter <code>new-crawl</code> allows to force a new analysis:<br>
+        <a href="http://wikiwhere.west.uni-koblenz.de/article.php?url=https://de.wikipedia.org/wiki/Test&new-crawl=true">http://wikiwhere.west.uni-koblenz.de/article.php?url=https://de.wikipedia.org/wiki/Test&new-crawl=true</a><br>
         Again, the analysis can take several minutes, depending on the number of external links in the article.
       </p>
-
-
+      <h4>Via the File Browser</h4>
+      <p>
+        Previous analyses can be accessed via the <a href="http://wikiwhere.west.uni-koblenz.de/articles">Articles</a> tab on the website.
+        The results are stored in a folder according to the wikipedia language edition.<br>
+        For example, the analysis results for the German Wikipedia article <a href="https://de.wikipedia.org/wiki/Test">Test</a> can be found at: <a href="http://wikiwhere.west.uni-koblenz.de/articles/de/Test/">http://wikiwhere.west.uni-koblenz.de/articles/de/Test/</a>
+      </p>
+      <h4>Via wget</h4>
+      <p>
+        In order to retrieve the analysis.json file with wget, the following command can be used:<br>
+        <code>wget "http://wikiwhere.west.uni-koblenz.de/json.php?url=[article-url]" -O [file-name].json</code><br>
+        Again, it is possible to use the <code>new-crawl</code> parameter to force a new analysis.<br>
+        A concrete example for the German Wikipedia article <a href="https://de.wikipedia.org/wiki/Test">Test</a>:<br>
+        <code>wget "http://wikiwhere.west.uni-koblenz.de/json.php?url=https://de.wikipedia.org/wiki/Test" -O de-Test.json</code><br>
+      </p>
 
       <h3>Source Code</h3>
       <p>
-        The source code for this website is on GitHub at <a href="https://github.com/mkrnr/wikiwhere-website">https://github.com/mkrnr/wikiwhere-website</a>.
-        For the analysis we have written Python modules which are also on Github at <a href="https://github.com/mkrnr/wikiwhere">https://github.com/mkrnr/wikiwhere</a>.
+        The source code for this website is on GitHub at: <a href="https://github.com/mkrnr/wikiwhere-website">https://github.com/mkrnr/wikiwhere-website</a><br>
+        For the analysis we have written Python modules which are also on Github at: <a href="https://github.com/mkrnr/wikiwhere">https://github.com/mkrnr/wikiwhere</a>
       </p>
       <h2>The Team</h2>
 
