cd7fb8a21ebfae1a1b0ea6d729be1b0d57197a8c
mkrnr <info@mkoerner.de>
2016-05-17 15:16:00 +0200

    Replace pie chart with bar chart

--- article.php
+++ article.php
@@ -22,6 +22,8 @@
     <script src="http://d3js.org/topojson.v1.min.js"></script>
     <script src="http://datamaps.github.io/scripts/datamaps.world.min.js?v=1"></script>
 
+    <script src="http://labratrevenge.com/d3-tip/javascripts/d3.tip.v0.6.3.js"></script>
+
     <script>
       function toJson() {
         var url = window.location.href ;
@@ -29,7 +31,6 @@
         window.location = load;
       }
     </script>
-
 
     <title>wikiwhere - <?php echo $article_url;?></title>
   </head>
@@ -44,7 +45,7 @@
         href="#" onclick="toJson();return false;">Get JSON file</a>
 
     <div id="map" align="center"></div>
-    <div id="pie" align="center"></div>
+    <div id="bar" align="center"></div>
     <div id="table" align="center"></div>
     </div>
     <?php include "php/get-article.php";?>
@@ -125,60 +126,85 @@
     });
      </script>
 
-    <script>
-	var data;
+     <script>
 
-	//var article_counts_path = article_path;
+     var margin = {top: 40, right: 20, bottom: 30, left: 40},
+         width = 960 - margin.left - margin.right,
+         height = 500 - margin.top - margin.bottom;
 
-	d3.json("<?php echo $article_counts_path; ?>", function(dataset) {
-    data = dataset;
+     ;
 
-	var width = 1000,
-	  height = 400,
-	  radius = Math.min(width, height) / 2;
+     var x = d3.scale.ordinal()
+         .rangeRoundBands([0, width], .1);
 
-	var color = d3.scale.category20();
+     var y = d3.scale.linear()
+         .range([height, 0]);
 
-	var arc = d3.svg.arc()
-	  .outerRadius(radius - 10)
-	  .innerRadius(radius - 70);
+     var xAxis = d3.svg.axis()
+         .scale(x)
+         .orient("bottom");
 
-	var pie = d3.layout.pie()
-	  .sort(null)
-	  .value(function(d) {
-		return d.count;
-	  });
+     var yAxis = d3.svg.axis()
+         .scale(y)
+         .orient("left")
 
 
+     var tip = d3.tip()
+       .attr('class', 'd3-tip')
+       .offset([-10, 0])
+       .html(function(d) {
+         return "<strong>Frequency:</strong> <span style='color:red'>" + d.count + "</span>";
+       })
 
-	var svg = d3.select("#pie").append("svg")
-	  .attr("width", width)
-	  .attr("height", height)
-	  .append("g")
-	  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+     var svg = d3.select("#bar").append("svg")
+         .attr("width", width + margin.left + margin.right)
+         .attr("height", height + margin.top + margin.bottom)
+       .append("g")
+         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	var g = svg.selectAll(".arc")
-	  .data(pie(data))
-	  .enter().append("g")
-	  .attr("class", "arc");
+     svg.call(tip);
 
-	g.append("path")
-	  .attr("d", arc)
-	  .style("fill", function(d) {
-		return color(d.data.label);
-	  });
+     d3.json("<?php echo $article_counts_path; ?>", function(data) {
+       x.domain(data.map(function(d) { return d.label; }));
+       y.domain([0, d3.max(data, function(d) { return d.count; })]);
 
-	g.append("text")
-	  .attr("transform", function(d) {
-		return "translate(" + arc.centroid(d) + ")";
-	  })
-	  .attr("dy", ".35em")
-	  .style("text-anchor", "middle")
-	  .text(function(d) {
-		return d.data.label+": "+d.data.count;
-	  });
-	  });
-    </script>
+       svg.append("g")
+           .attr("class", "x axis")
+           .attr("transform", "translate(0," + height + ")")
+           .call(xAxis);
+
+       svg.append("g")
+           .attr("class", "y axis")
+           .call(yAxis)
+         .append("text")
+           .attr("transform", "rotate(-90)")
+           .attr("y", 6)
+           .attr("dy", ".71em")
+           .style("text-anchor", "end")
+           .text("Frequency");
+
+       svg.selectAll(".bar")
+           .data(data)
+         .enter().append("rect")
+           .attr("class", "bar")
+           .attr("x", function(d) { return x(d.label); })
+           .attr("width", x.rangeBand())
+           .attr("y", function(d) { return y(d.count); })
+           .attr("height", function(d) { return height - y(d.count); })
+           .on('mouseover', tip.show)
+           .on('mouseout', tip.hide)
+
+     });
+
+     function type(d) {
+       d.count = +d.count;
+       return d;
+     }
+
+     </script>
+
+
+
     <script>
         d3.json("<?php echo $article_analysis_path; ?>", function (error, data){
 
